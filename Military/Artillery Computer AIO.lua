f7f509aa333515d5711e8ef1f192ae7aacf11e1c
Cmonster820 <72608260+Cmonster820@users.noreply.github.com>
2025-12-08 18:07:20 -0500

    buh

--- Military/Artillery Computer AIO.lua
+++ Military/Artillery Computer AIO.lua
@@ -7,29 +7,40 @@
 --this assumes you have an autoloader that resets the angle after each shot and that it takes 5 seconds to go
 --SET UP AZIMUTH WITH 8:1 RATIO
 math = require("math")
+function round(n)
+    if math.ceil(n)-n>math.abs(math.floor(n)) then
+        return math.floor(n)
+    end
+    return math.ceil(n)
+end
 print("Scanning for data file")
 if fs.exists("/data/artillery.txt") then
     print("File found")
-    datafile = io.open("/data/artillery.txt","r")
-    x = datafile:read("l").tonumber()
-    y = datafile:read("l").tonumber()
-    z = datafile:read("l").tonumber()
-    length = datafile:read("l").tonumber()
-    v = datafile:read("l").tonumber()
+    datafile = io.open("/data/artillery.txt","r+")
+    x = tonumber(datafile:read("l"))
+    print(x)
+    y = tonumber(datafile:read("l"))
+    print(y)
+    z = tonumber(datafile:read("l"))
+    print(z)
+    length = tonumber(datafile:read("l"))
+    print(length)
+    v = tonumber(datafile:read("l"))
+    print(v)
     datafile:close()
     goto datadone
 end
 ::reask::
 print("What is the X co-ordinate of the cannon's elevation actuation point?")
-x = read().tonumber()
+x = tonumber(read())
 print("What is the Y co-ordinate of the cannon's elevation actuation point?")
-y = read().tonumber()
+y = tonumber(read())
 print("What is the Z co-ordinate of the cannon's elevation actuation point?")
-z = read().tonumber()
+z = tonumber(read())
 print("The co-ordinates of the cannon's elevation actuation point is ("..x..","..y..","..z.."). Is this correct? (Y/N)")
 answer = read()
 ::recorrect::
-if answer = "N" then
+if answer == "N" then
     goto reask
 elseif answer ~="Y" then
     print("Invalid response, please use Y or N")
@@ -37,20 +48,21 @@ elseif answer ~="Y" then
     goto recorrect
 end
 print("What is the length of the cannon's barrel?")
-length = read().tonumber()
+length = tonumber(read())
 v = 0
 print("How many brass cartridge thingies are in use?")
-v=read().tonumber()*120+v
+v=tonumber(read())*120+v
 print("How many powder things are in use")
-v=read().tonumber()*40+v
+v=tonumber(read())*40+v
 print("Understood. Writing data to file.")
-datafile = io.open("/data/artillery.txt","a")
-datafile:write(x+"\n")
-datafile:write(y+"\n")
-datafile:write(z+"\n")
-datafile:write(length+"\n")
-datafile:write(v+"\n")
+datafile = io.open("/data/artillery.txt","w")
+datafile:write(x.."\n")
+datafile:write(y.."\n")
+datafile:write(z.."\n")
+datafile:write(length.."\n")
+datafile:write((v/20).."\n")
 datafile:close()
+v = v/20
 print("Datafile generated and data stored")
 ::datadone::
 azimuth = peripheral.wrap("Create_SequencedGearshift_1")
@@ -59,11 +71,11 @@ print("System ready")
 ::systemready::
 print("Please input the co-ordinates of the target")
 print("X")
-tx = read().tonumber()
+tx = tonumber(read())
 print("Y")
-ty = read().tonumber()
+ty = tonumber(read())
 print("Z")
-tz = read().tonumber()
+tz = tonumber(read())
 tvx = tx-x
 tvy = ty-y
 tvz = tz-z
@@ -74,36 +86,42 @@ print("Distance: "..dist.." blocks, rounded to nearest integer")
 print("Trajectory projected to 2 dimensions")
 theta = 0
 gravity = -1 --(I think this is blocks/tick/tick idk)
+--calulate maximum height
+maxheight = ((v*math.sin(1/2*math.asin((dist*20*gravity)/(v^2))))^2)/(2*20*gravity)
 Cd = 0.99 --velocity remaining after each tick (%)
+oldendx = 0
+oldendy = 0
+endx = 0
+endy = 0
 for thetas=0,math.rad(90),math.rad(0.125) do
     startx = math.cos(thetas)*length
     starty = math.sin(thetas)*length
-    oldendx = 0
-    oldendy = 0
-    endx = 0
-    endy = 0
     projx = startx
     projy = starty
     projv = v
     prevprojy = starty
     prevthetas = thetas-0.125
-    projang = theta
+    projang = thetas
     while true do
         projvx = projv*math.cos(projang)
         projvy = projv*math.sin(projang)
         projx = projx + projvx
         projy = projy + projvy
         projvy = projvy+gravity
-        projvx = projvx*(Cd*math.cos(projang))
-        projvy = projvy*(Cd*math.sin(projang))
+        projvx = projvx*(Cd*math.abs(math.cos(projang)))
+        projvy = projvy*(Cd*math.abs(math.sin(projang)))
         projv = math.sqrt(projvy^2+projvx^2)
-        projang = math.arctan(projvy/projvx)
-        print("vx: "..projvx.." vy: "..projvy.." x: "..projx.." y: "..projy.." v: "..projv.." θp: "..projang.." θ: "..thetas)
+        projang = math.atan(projvy/projvx)
+        print("vx: "..projvx.."\nvy: "..projvy.."\nx: "..projx.."\ny: "..projy.."\nv: "..projv.."\nThetap: "..projang.."\nTheta: "..thetas.."\n")
         if projx>=dist then
             endx=projx
             break
         end
-        if projy<=tvy and projx>=dist then
+        if projy<=tvy then
+            endy=projy
+            break
+        end
+        if projy>=maxheight then
             endy=projy
             break
         end
@@ -113,10 +131,17 @@ for thetas=0,math.rad(90),math.rad(0.125) do
         print("Firing solution found for θ = "..math.deg(theta))
         break
     end
-    if (dist-oldendx<dist-endx and tvy-oldendy<tvy-endy) then
-        theta = prevthetas
-        print("Firing solution found for θ = "..math.deg(theta).."\nNOTE: Firing solution not exact")
-        break
+    if (oldendx<dist and dist<endx) then
+        if (dist-oldendx<dist-endx and tvy-oldendy<tvy-endy) then
+            theta = prevthetas
+            print("Firing solution found for θ = "..math.deg(theta).."\nNOTE: Firing solution not exact")
+            break
+        end
+        if (dist-oldendx>dist-endx and tvy-oldendy>tvy-endy) then
+            theta = thetas
+            print("Firing solution found for θ = "..math.deg(theta).."\nNOTE: Firing solution not exact")
+            break
+        end
     end
     oldendy = endy
     oldendx = endx
@@ -124,9 +149,9 @@ end
 print("Calculating φ")
 phi = 0
 if tvx>0 then
-    phi = math.arctan(tvz/tvx)
+    phi = math.atan(tvz/tvx)
 else
-    phi = math.arctan(tvz/tvx)+math.rad(180)
+    phi = math.atan(tvz/tvx)+math.rad(180)
 end
 if phi<0 then
     phi = phi+math.rad(360)
@@ -134,15 +159,15 @@ end
 print("φ = "..math.deg(phi))
 print("Firing solution (φ,θ): ("..phi..","..theta..")")
 print("Aiming.")
-azimuth.rotate(math.round(8*math.deg(phi)),2)
-elevation.rotate(math.round(8*math.deg(theta)),2)
+azimuth.rotate(math.floor(8*math.deg(phi)),2)
+elevation.rotate(math.floor(8*math.deg(theta)),2)
 while azimuth.isRunning() and elevation.isRunning do
     os.sleep(0.1)
 end
 print("Aiming Complete, firing")
-redstone.setOutput("top",1)
+redstone.setOutput("top",true)
 os.sleep(1)
-redstone.setOutput("top",0)
+redstone.setOutput("top",false)
 print("Firing Complete, awaiting autoloading")
 os.sleep(10)
 print("Autoloading timer complete, ready for reuse")
