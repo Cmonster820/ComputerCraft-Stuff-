6b0ff0c9aad738eabae1995a33061a9b43fe1b18
Cmonster820 <72608260+Cmonster820@users.noreply.github.com>
2025-12-09 08:19:11 -0500

    not done

--- Military/Artillery Computer Client.lua
+++ Military/Artillery Computer Client.lua
@@ -1,5 +1,13 @@
 --for pocket computers or whatever they're called, requires wireless or ender modem version, requires advanced version for touchscreen
 h,w = term.native().getSize()
+artyPacket = 
+{
+    name="admin",
+    type = "",
+    secret = "",
+    target = {},
+    data = ""
+}
 main = term.native()
 m = peripheral.find("modem")
 log = window.create(term.current(),math.round(w*0.6),2,math.round(w-math.round(w*0.6))-1,math.round(h*0.2))
@@ -16,3 +24,14 @@ rednet.open(m)
 print("Finding host")
 hostID = rednet.lookup("Arty", host)
 print("Found, Host ID: "..hostID)
+print("Connecting...")
+packet.data = "connect"
+rednet.send(hostID,textutils.serialize(packet),"arty")
+packet.data = ""
+_,message = rednet.receive("arty")
+message = textutils.unserialize(message)
+if message.data == "Allowed" then
+    print("Granted")
+else
+    error("Denied")
+end
