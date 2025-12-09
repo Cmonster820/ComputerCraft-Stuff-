4e8bdb29f1886c15a323b2ff8240a6ad7edc6829
Cmonster820 <72608260+Cmonster820@users.noreply.github.com>
2025-12-09 12:02:23 -0500

    not done

--- General/Fax system.lua
+++ General/Fax system.lua
@@ -1,6 +1,8 @@
 p = peripheral.find("printer")
 m = peripheral.find("modem")
-rednet.host("FAX","end1")
+name = "end1"
+rednet.host("FAX",name)
+rednet.host("FAXint",name)
 ID = rednet.getIdentifier()
 w,h = p.getPageSize()
 if not rednet.isOpen(m) then
@@ -16,6 +18,35 @@ packet =
     message = ""
 }
 function main()
+    local selected = false
+    local dest = 0
+    local destn = ""
+    while not selected do
+        print("Where do you want to send a message")
+        print(rednet.lookup("FAX"))
+        local input = tonumber(read())
+        rednet.send(input,"Name?","FAXint")
+        local source,message = rednet.receive("FAXint")
+        if source~=input then
+            error("Something's wrong, security compromised")
+        end
+        ::reconfirm::
+        print("You have selected: "..message.."\nIs this correct? (Y/N)")
+        local conf = read()
+        if conf=="N" then
+            print("Resetting...")
+        elseif conf == "Y" then
+            print("Confirmed")
+            selected = true
+            dest = input
+            destn = message
+        else
+            print("Invalid response, retrying")
+        end
+    end
+    
 end
 function parseMessage(message,from)
+end
+function listener()
 end
