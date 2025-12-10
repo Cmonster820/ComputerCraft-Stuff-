fb6e4ffbb7eff364cd3a57971cb0b7fc5c5dd3ab
Cmonster820 <72608260+Cmonster820@users.noreply.github.com>
2025-12-10 14:59:54 -0500

    Idea

--- General/TTS.lua
+++ General/TTS.lua
@@ -1,5 +1,5 @@
 --objective: create a request to a tts website to say words and then convert that to dfpwm, then say it
---uses some free tts service I can find
+--new idea: connect to a server hosted on my spare computer in real life, which receives a message with the text (and maybe some other data) and then uses that to generate a tts file, downloads it, uses ffmpeg to convert to dfpwm, and sends the file to the computer
 local dfpwm = require("cc.audio.dfpwm")
 local speaker = peripheral.find("speaker")
 local decoder = dfpwm.make_decoder()
