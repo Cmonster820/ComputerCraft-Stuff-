ecd3d479f1d8d5acc446a032eb63f2e81d35a52c
Cmonster820 <72608260+Cmonster820@users.noreply.github.com>
2025-12-11 11:33:31 -0500

    not done

--- Websocket stuff/TTSserver.py
+++ Websocket stuff/TTSserver.py
@@ -6,4 +6,39 @@ import asyncio
 import websockets
 from google.cloud import texttospeech
 import ffmpeg
-#no time to actually write this right now I'll do it later
+async def synthesize_message(text):
+    client = texttospeech.TextToSpeechClient()
+    sythIn = texttospeech.SynthesisInput(text = text)
+    #the following 3 blocks are copypasted from google ai (like the rest of this program because I haven't used Python for a real project yet)
+    voice = texttospeech.VoiceSelectionParams(
+        language_code="en-US", 
+        ssml_gender=texttospeech.SsmlVoiceGender.NEUTRAL
+    )
+    audio_config = texttospeech.AudioConfig(
+        audio_encoding=texttospeech.AudioEncoding.MP3
+    )
+    #synth response
+    response = client.synthesize_speech(
+        input=synthesis_input, voice=voice, audio_config=audio_config
+    )
+    with open("C:/CCTTS/"+text+".mp3","wb") as out:
+        out.write(response.audio_content)
+    print(f"Audio written to: C:/CCTTS/{text}.mp3")
+    #differs from google ai here because it didn't have conversion to dfpwm in the same file
+    return response
+#voice choice decoder goes here, not implemented yet
+async def convertMP3DfPWM(audioMP3,filename):
+    try:
+        (
+            ffmpeg
+            .input(audioMP3)
+            .output(filename)
+        )
+async def websocket_listener():
+    uri = ""#haven't set this up yet
+    async with websockets.connect(uri) as websocket:
+        print("Waiting for message...")
+        async for message in websocket:
+            print(f"Received message: {message}")
+            audioMP3 = await synthesize_message(message)
+            audioDfPWM = await convertMP3DfPWM(audioMP3,"C:/CCTTS/"+message+".dfpwm")
