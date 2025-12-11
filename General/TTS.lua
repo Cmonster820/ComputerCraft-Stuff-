14101c74503b68a14260c1ccac94a019bdfd085a
Cmonster820 <72608260+Cmonster820@users.noreply.github.com>
2025-12-11 08:24:27 -0500

    not done

--- General/TTS.lua
+++ General/TTS.lua
@@ -3,7 +3,30 @@
 local dfpwm = require("cc.audio.dfpwm")
 local speaker = peripheral.find("speaker")
 local decoder = dfpwm.make_decoder()
+local ID = rednet.getIdentifier()
+--obtain message
+print("What would you like to say?")
+local content = read()
+print("How would you like it said? (number, 0 for default)")
+local ctype = tonumber(read())
+local packet = {
+    content = "",
+    ctype = 0
+}
+--types will be added later
 
+--Connect to websocket and send message
+function sendWsMessage(message)
+    local ws = assert(http.websocket(--[[add in the websocket later]],{message = "Connecting, ID: "..ID}), "Websocket unavailable") --ensure websocket is there
+    local message,bin = ws.receive()
+    assert(bin == false, "Binary response received, not supported")
+    assert(message == "Accepted", "Connection declined by server")
+    print("Sending packet")
+    packet.content = content
+    packet.ctype = ctype
+    ws.send(textutils.serialize(packet))
+    print("Done")
+end
 for chunk in io.lines("data/audio.dfpwm",16*1024) do
     local buffer = decoder(chunk)
     while not speaker.playAudio(buffer) do
