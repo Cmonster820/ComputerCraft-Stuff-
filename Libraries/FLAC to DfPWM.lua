db903b9a93f002b3f7d15197359cc5aff6c98dc5
Cmonster820 <72608260+Cmonster820@users.noreply.github.com>
2026-01-30 09:44:28 -0500

    imports

--- Libraries/FLAC to DfPWM.lua
+++ Libraries/FLAC to DfPWM.lua
@@ -1,5 +1,9 @@
 --this is probably the worst idea I've ever had
 local main = {} --create library table returned when this is required
+local io = require("io")
+local fs = require("fs")
+local math = require("math") 
+local bit32 = require("bit32")
 local function parseFileHeader(header)
     
 end
@@ -7,6 +11,7 @@ local function parseMetadataBlockHeader(header)
 
 end
 local function main.convertFile(pathIn, pathOut)
+    local fileIn = io.open(pathIn, "r+")
 
 end
 return main
