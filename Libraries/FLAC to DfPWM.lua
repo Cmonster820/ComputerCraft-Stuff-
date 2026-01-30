5808305700dad37ae3a25dff659cd73448b05717
Cmonster820 <72608260+Cmonster820@users.noreply.github.com>
2026-01-30 09:38:21 -0500

    get first few necessary functions down

--- Libraries/FLAC to DfPWM.lua
+++ Libraries/FLAC to DfPWM.lua
@@ -1,1 +1,11 @@
 --this is probably the worst idea I've ever had
+local main = {} --create library table returned when this is required
+local function parseFileHeader(header)
+    
+end
+local function parseMetadataBlockHeader(header)
+
+end
+local function main.convertFile(pathIn, pathOut)
+
+end
