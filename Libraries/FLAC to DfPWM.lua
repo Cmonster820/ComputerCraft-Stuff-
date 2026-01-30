76b2a65065ab9f869ab011670cbd95ba4d26ba36
Cmonster820 <72608260+Cmonster820@users.noreply.github.com>
2026-01-30 13:37:42 -0500

    not done

--- Libraries/FLAC to DfPWM.lua
+++ Libraries/FLAC to DfPWM.lua
@@ -4,14 +4,16 @@ local io = require("io")
 local fs = require("fs")
 local math = require("math") 
 local bit32 = require("bit32")
-local function parseFileHeader(header)
-    
+local function parseStreamInfo(blockHeader)
+
 end
-local function parseMetadataBlockHeader(header)
+local function parseBlockHeader(header)
 
 end
 local function main.convertFile(pathIn, pathOut)
     local fileIn = io.open(pathIn, "r+")
-
+    local signature = fileIn:read(4)
+    assert(signature=="fLaC", "Not of type fLaC") --Check file signature for correct thing
+    local info = fileIn:read(34)
 end
 return main
