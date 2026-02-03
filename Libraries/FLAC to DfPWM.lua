2f4dbf642a10eaef6410e21c0a3cba9f0c61fd95
Cmonster820 <72608260+Cmonster820@users.noreply.github.com>
2026-02-03 08:23:15 -0500

    not done

--- Libraries/FLAC to DfPWM.lua
+++ Libraries/FLAC to DfPWM.lua
@@ -4,8 +4,16 @@ local io = require("io")
 local fs = require("fs")
 local math = require("math") 
 local bit32 = require("bit32")
+local lshift(), rshift() = bit32.lshift(), bit32.rshift()
 local function parseStreamInfo(Block)
-
+    local minSize = tonumber(string.sub(Block, 1,2))
+    local maxSize = tonumber(string.sub(Block, 3,4))
+    local minFrameSize = tonumber(string.sub(Block,5,7))
+    local maxFrameSize = tonumber(string.sub(Block,8,10))
+    local rate = rshift(tonumber(string.sub(Block,11,13)),4)
+    local channels = rshift(lshift(tonumber(string.sub(Block,13,13)),4),5)
+    local bitsPsample = ((tonumber(string.sub(Block,13,13))%2)*16)+(rshift(tonumber(string.sub(Block,14,14)),4))
+    
 end
 local function parseBlockHeader(header)
 
@@ -15,6 +23,6 @@ local function main.convertFile(pathIn, pathOut)
     local signature = fileIn:read(4)
     assert(signature=="fLaC", "Not of type fLaC") --Check file signature for correct thing
     local StreamInfo = fileIn:read(34)
-    local minSize, maxSize, minFrameSize, rate, channels, bitsPsample, totalInterchannelSamples, MD5Checksum =parseStreamInfo(StreamInfo)
+    local minSize, maxSize, minFrameSize, maxFrameSize, rate, channels, bitsPsample, totalInterchannelSamples, MD5Checksum =parseStreamInfo(StreamInfo)
 end
 return main
