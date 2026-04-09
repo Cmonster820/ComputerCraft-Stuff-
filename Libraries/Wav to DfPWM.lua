08031b93d2521dd85dbc9a0a7fdb4e448b9fdd6c
Cooper Howard <72608260+Cmonster820@users.noreply.github.com>
2026-04-09 14:09:05 -0400

    idrw do this rn

---
+++ Libraries/Wav to DfPWM.lua
@@ -0,0 +1,15 @@
+local main = {}
+local io = require("io")
+local fs = require("fs")
+local math = require("math") 
+local bit32 = require("bit32")
+local dfpwm = require("cc.audio.dfpwm")
+
+local function dochunk(chunk, encoder, fileout)
+
+end
+local function convert(fileIn, fileOut)
+    
+end
+
+return main
