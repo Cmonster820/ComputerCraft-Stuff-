e0da74f6bf7d8a30f84cf02b2b11e240af639fcc
Cooper Howard <72608260+Cmonster820@users.noreply.github.com>
2026-04-10 08:53:20 -0400

    some of the words in the spec don't make sense

--- Libraries/Wav to DfPWM.lua
+++ Libraries/Wav to DfPWM.lua
@@ -4,14 +4,29 @@ local fs = require("fs")
 local math = require("math") 
 local bit32 = require("bit32")
 local dfpwm = require("cc.audio.dfpwm")
+local lshift(), rshift(), bor(), band() = bit32.lshift(), bit32.rshift(), bit32.bor(), bit32.band()
+local function getbytes(str, start, endi)
+    local bytes = {string.byte(str,start,endi)}
+    local out = 0
+    for i = 1, #bytes do
+        out = bor(out,lshift(bytes[i],8))
+    end
+    out = bor(out,bytes[#bytes])
+    return out
+end
+local function parseHeader(header)
+    local ckID = header:sub(1,4)
+    local cksize = getbytes(header,5,9)
+    local WAVEID
 
+end
 
-
-local function convert(pathIn, pathOut)
+local function main.convert(pathIn, pathOut)
     local fileIn = io.open(pathIn,"rb")
     local fileOut = io.open(pathOut, "wb")
     local encoder = dfpwm.make_encoder()
-    local header = 
+    local header = fileIn:read(12)
+
 end
 
 return main
